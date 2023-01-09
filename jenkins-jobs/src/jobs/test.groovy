9561b6bf4e6cd25470d0b7b3ce92741a6e8ed3d4
rishidha <hrishidhagupta@gmail.com>
2023-01-09 11:32:54 +0530

    updating test.groovy

--- jenkins-jobs/src/jobs/test.groovy
+++ jenkins-jobs/src/jobs/test.groovy
@@ -1,6 +1,11 @@
 mavenJob('jenkins tutorial demo - Simple DSL job'){
     description'A very simple demo for job dsl'
     logRotator{
-        numToKee 5
+        numToKeep 5
+    }
+    parameters {
+        gitParam('Branch')
+        description'The git branch to checkout'
+        defaultValue 'origin/master'
     }
 }
